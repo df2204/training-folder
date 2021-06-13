4661f125e842610404cf2c3e6830d78d0498fa26
df2204 <dmfe2204@gmail.com>
2021-06-13 20:49:47 +0200

    home assignment1

--- Smart calc/calc.py
+++ Smart calc/calc.py
@@ -1,4 +1,29 @@
 print("hello")
-x= input ("x = ")
-y= input ("y = ")
-print (x+y)
+o= input ("input operation ")
+x= int(input ("x = "))
+y= int(input ("y = "))
+if o == "+": 
+    print (x+y)
+elif o == "-":
+    print (x-y)
+elif o == "*" :
+    print (x*y)
+elif o == "/" : 
+    if y==0 : 
+        print ("it's a simple calc, don't be too smart")
+    else :
+        print (x/y)
+elif o == "//" :
+    if y==0 : 
+        print ("it's a simple calc, don't be too smart")
+    else :
+        print (x//y)
+elif o == "**" :
+    print (x**y)
+elif o == "%" :
+    if y==0 : 
+        print ("it's a simple calc, don't be too smart")
+    else :
+        print (x%y)
+else :
+    print ("Unknown operational symbol. It's a simple calc, don't forget" )
